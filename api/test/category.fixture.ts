193db3ef0c8b175346355fd52af8e1245ea2e9cd
Gordian <arragon05@gmail.com>
2019-07-29 14:40:56 +0200

    get and post category

--- api/test/category.fixture.ts
+++ api/test/category.fixture.ts
@@ -2,7 +2,7 @@ import * as faker from 'faker/locale/fr';
 import { CategoriesService } from '../src/api/categories/categories.service';
 import { CategoryDTO } from '../src/api/categories/category.dto';
 
-export async function insertArticleData(categoryService: CategoriesService) {
+export async function insertCategoryData(categoryService: CategoriesService) {
   // populate mongo
   for (let i = 1; i <= 50; i++) {
     const categoryDTO: CategoryDTO = {title: faker.lorem.sentence(50)};
--- api/test/main.ts
+++ api/test/main.ts
@@ -1,6 +1,8 @@
 import { startInMemoryDB } from './in-memory-db';
 import { insertArticleData } from './article.fixture';
 import { ArticlesService } from '../src/api/articles/articles.service';
+import { CategoriesService } from '../src/api/categories/categories.service';
+import { insertCategoryData } from './category.fixture';
 
 async function bootstrap() {
   await startInMemoryDB();
@@ -9,5 +11,8 @@ async function bootstrap() {
 
   const articlesService: ArticlesService = app.get('ArticlesService');
   insertArticleData(articlesService);
+
+  const categoriesService: CategoriesService = app.get('CategoriesService');
+  insertCategoryData(categoriesService);
 }
 bootstrap();
