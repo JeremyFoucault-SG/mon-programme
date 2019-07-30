948fe23402c059a9a2a4a257c016dba85759842a
mdhn <maxduchan@gmail.com>
2019-07-30 19:25:54 +0200

    add route and service

--- api/src/api/settings/settings.controller.ts
+++ api/src/api/settings/settings.controller.ts
@@ -1,17 +1,27 @@
 import { Controller, Get, Put, Body } from '@nestjs/common';
 import { SettingsModel } from './settings.model';
 import { SettingsDTO } from './settings.dto';
+import { ApiResponse, ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { SettingsService } from './settings.service';
+import { User } from '../../decorators/user.decorator';
 
 @Controller('settings')
 export class SettingsController {
 
+    constructor(private readonly settingsService: SettingsService) {}
+
     @Get()
-    async readOne(): Promise<SettingsModel> {
-        return null;
+    @ApiBearerAuth()
+    @ApiOperation({ title: 'Get settings for current authenticated user' })
+    @ApiResponse({ status: 200, description: 'Return settings.' })
+    @ApiResponse({ status: 404, description: 'Not Found.' })
+    @ApiResponse({ status: 403, description: 'Forbidden.' })
+    async readOne(@User('id') idUser: string): Promise<SettingsModel> {
+        return this.settingsService.find(idUser);
     }
 
     @Put()
-    async update(@Body() settings: SettingsDTO): Promise<SettingsModel> {
+    async update(@User('id') idUser: string, @Body() settings: SettingsDTO): Promise<SettingsModel> {
         return null;
     }
 }
