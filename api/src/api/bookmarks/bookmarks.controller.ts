e1f947f8dbf4d870580ec1dc68c46e20c06a83ad
mdhn <maxduchan@gmail.com>
2019-07-30 19:24:00 +0200

    add route and service

--- api/src/api/bookmarks/bookmarks.controller.ts
+++ api/src/api/bookmarks/bookmarks.controller.ts
@@ -1,22 +1,36 @@
 import { Controller, Get, Post, Delete, HttpCode, HttpStatus, Body, Param } from '@nestjs/common';
 import { BookmarkModel } from './bookmark.model';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { User } from '../../decorators/user.decorator';
+import { BookmarksService } from './bookmarks.service';
+import { BookmarkDTO } from './bookmark.dto';
 
 @Controller('bookmarks')
 export class BookmarksController {
 
-    @Post()
-    async create(@Body() bookmark: BookmarkModel): Promise<BookmarkModel> {
-        return null;
-    }
+  constructor(private readonly bookmarksServices: BookmarksService) { }
 
-    @Get()
-    async readAll(): Promise<BookmarkModel> {
-        return null;
-    }
+  @Post()
+  @ApiBearerAuth()
+  @ApiOperation({ title: 'Create new body for current authenticated user' })
+  @ApiResponse({ status: 201, description: 'Return body.' })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async create(@User('id') idUser: string, @Body() bookmark: BookmarkDTO): Promise<BookmarkModel> {
+    return this.bookmarksServices.insert(idUser, bookmark);
+  }
 
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    async delete(@Param('id') idBookmark: string): Promise<void> {
-        return null;
-    }
+  @Get()
+  @ApiBearerAuth()
+  @ApiOperation({ title: 'Get all bookmarks of current authenticated user' })
+  @ApiResponse({ status: 200, description: 'Return an array of bookmarks.' })
+  async readAll(@User('id') idUser: string): Promise<BookmarkModel[]> {
+    return this.bookmarksServices.findAll(idUser);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@User('id') idUser: string, @Param('id') idBookmark: string): Promise<void> {
+    return null;
+  }
 }
