295a2860d7c19be93881efc2ad36a5e9999f283c
mdhn <maxduchan@gmail.com>
2019-07-30 19:25:46 +0200

    add route and service

--- api/src/api/followed-coachings/followed-coachings.controller.ts
+++ api/src/api/followed-coachings/followed-coachings.controller.ts
@@ -1,27 +1,44 @@
 import { Controller, Post, Get, Put, Param, Body } from '@nestjs/common';
 import { FollowedCoachingModel } from './followed-coaching.model';
 import { FollowedCoachingDTO } from './followed-coachings.dto';
+import { FollowedCoachingsService } from './followed-coachings.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { User } from '../../decorators/user.decorator';
 
 @Controller('followed-coachings')
 export class FollowedCoachingsController {
 
-    @Post()
-    async create(@Body() coaching: FollowedCoachingDTO): Promise<FollowedCoachingModel> {
-        return null;
-    }
+  constructor(private readonly followedCoachingsService: FollowedCoachingsService) {}
 
-    @Get(':id')
-    async readOne(@Param('id') idCoaching: string): Promise<FollowedCoachingModel> {
-        return null;
-    }
+  @Post()
+  @ApiBearerAuth()
+  @ApiOperation({ title: 'Create new followed coaching for current authenticated user' })
+  @ApiResponse({ status: 201, description: 'Return followed coaching.' })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async create(@User('id') idUser: string,  @Body() coaching: FollowedCoachingDTO): Promise<FollowedCoachingModel> {
+    return this.followedCoachingsService.insert(idUser, coaching);
+  }
 
-    @Get()
-    async readAll(): Promise<FollowedCoachingModel> {
-        return null;
-    }
+  @Get(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ title: 'Get followed coaching by ID of current authenticated user' })
+  @ApiResponse({ status: 200, description: 'Return followed coaching.' })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
+  async readOne(@User('id') idUser: string, @Param('id') idCoaching: string): Promise<FollowedCoachingModel> {
+    return this.followedCoachingsService.findById(idUser, idCoaching);
+  }
 
-    @Put(':id')
-    async update(@Param('id') idCoaching: string, @Body() coaching: FollowedCoachingDTO): Promise<FollowedCoachingModel> {
-        return null;
-    }
+  @Get()
+  @ApiBearerAuth()
+  @ApiOperation({ title: 'Get all bodies of current authenticated user' })
+  @ApiResponse({ status: 200, description: 'Return an array of bodies.' })
+  async readAll(@User('id') idUser: string): Promise<FollowedCoachingModel[]> {
+    return this.followedCoachingsService.findAll(idUser);
+  }
+
+  @Put(':id')
+  async update(@User('id') idUser: string, @Param('id') idCoaching: string, @Body() coaching: FollowedCoachingDTO): Promise<FollowedCoachingModel> {
+    return null;
+  }
 }
