164d582af0e16623fcf1c83085ac44b20173247a
mdhn <maxduchan@gmail.com>
2019-07-30 19:25:09 +0200

    add route and service

--- api/src/api/wishes/wishes.module.ts
+++ api/src/api/wishes/wishes.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { WishesController } from './wishes.controller';
 import { WishesService } from './wishes.service';
+import { UsersModule } from '../users/users.module';
+import { WishModel } from './wish.model';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { CoachingsModule } from '../coachings/coachings.module';
 
 @Module({
+  imports: [
+    TypegooseModule.forFeature([WishModel]),
+    UsersModule,
+    CoachingsModule,
+  ],
   controllers: [WishesController],
   providers: [WishesService],
 })
