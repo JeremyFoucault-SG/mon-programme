dfc70a61e599c76d6f65c7a5d04840751e41929a
mdhn <maxduchan@gmail.com>
2019-07-30 19:23:05 +0200

    add route and service

--- api/src/api/bodies/bodies.controller.ts
+++ api/src/api/bodies/bodies.controller.ts
@@ -1,27 +1,44 @@
-import { Controller, Post, Get, Put, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Post, Get, Put, Body, Param } from '@nestjs/common';
 import { BodyModel } from './body.model';
 import { BodyDTO } from './body.dto';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { User } from '../../decorators/user.decorator';
+import { BodiesService } from './bodies.service';
 
 @Controller('bodies')
 export class BodiesController {
 
-    @Post()
-    async create(@Body() body: BodyDTO): Promise<BodyModel> {
-        return null;
-    }
+  constructor(private readonly bodiesService: BodiesService) {}
 
-    @Get(':id')
-    async readOne(@Param('id') idBody: string): Promise<BodyModel> {
-        return null;
-    }
+  @Post()
+  @ApiBearerAuth()
+  @ApiOperation({ title: 'Create new body for current authenticated user' })
+  @ApiResponse({ status: 201, description: 'Return body.' })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async create(@User('id') idUser: string,  @Body() body: BodyDTO): Promise<BodyModel> {
+    return this.bodiesService.insert(idUser, body);
+  }
 
-    @Get()
-    async readAll(): Promise<BodyModel> {
-        return null;
-    }
+  @Get(':id')
+  @ApiBearerAuth()
+  @ApiOperation({ title: 'Get body by ID of current authenticated user' })
+  @ApiResponse({ status: 200, description: 'Return body.' })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
+  async readOne(@User('id') idUser: string, @Param('id') idBody: string): Promise<BodyModel> {
+    return this.bodiesService.findById(idUser, idBody);
+  }
 
-    @Put(':id')
-    async update(@Param('id') idBody: string, @Body() body: BodyDTO): Promise<BodyModel> {
-        return null;
-    }
+  @Get()
+  @ApiBearerAuth()
+  @ApiOperation({ title: 'Get all bodies of current authenticated user' })
+  @ApiResponse({ status: 200, description: 'Return an array of bodies.' })
+  async readAll(@User('id') idUser: string): Promise<BodyModel[]> {
+    return this.bodiesService.findAll(idUser);
+  }
+
+  @Put(':id')
+  async update(@User('id') idUser: string, @Param('id') idBody: string, @Body() body: BodyDTO): Promise<BodyModel> {
+    return null;
+  }
 }
