e8add1ceafaac8db013422750c77691d13d7073f
mdhn <maxduchan@gmail.com>
2019-07-30 19:26:02 +0200

    add route and service

--- api/src/api/users/user.model.ts
+++ api/src/api/users/user.model.ts
@@ -1,14 +1,18 @@
 import {Typegoose, prop, arrayProp} from 'typegoose';
-import { Body } from '@nestjs/common';
 import { Types } from 'mongoose';
+import { Body } from '@nestjs/common';
 import { StatModel } from '../stats/stat.model';
 import { FollowedCoachingModel } from '../followed-coachings/followed-coaching.model';
 import { CartModel } from '../carts/cart.model';
 import { BookmarkModel } from '../bookmarks/bookmark.model';
 import { WishModel } from '../wishes/wish.model';
 import { SettingsModel } from '../settings/settings.model';
+import { BodyModel } from '../bodies/body.model';
+import { SubDocument, SubDocumentArray } from '../../../@types/typegoose';
+import { CoachingModel } from '../coachings/coaching.model';
 
 export class UserModel extends Typegoose {
+
     @prop()
     id: Types.ObjectId;
 
@@ -16,22 +20,22 @@ export class UserModel extends Typegoose {
     createdAt: Date;
 
     @arrayProp({items: Body})
-    bodies: Body[];
+    bodies: SubDocumentArray<BodyModel>;
 
     @arrayProp({items: StatModel})
-    stat: StatModel[];
+    stats: SubDocumentArray<StatModel>;
 
     @arrayProp({items: FollowedCoachingModel})
-    followedCoachings: FollowedCoachingModel[];
+    followedCoachings: SubDocumentArray<FollowedCoachingModel>;
 
     @arrayProp({items: CartModel})
-    carts: CartModel[];
+    carts: SubDocumentArray<CartModel>;
 
     @arrayProp({items: BookmarkModel})
-    bookmarks: BookmarkModel[];
+    bookmarks: SubDocumentArray<BookmarkModel>;
 
     @arrayProp({items: WishModel})
-    wishes: WishModel[];
+    wishes: SubDocumentArray<WishModel>;
 
     @prop({_id: false})
     settings: SettingsModel;
--- api/src/api/users/users.service.ts
+++ api/src/api/users/users.service.ts
@@ -4,25 +4,29 @@ import { UserModel } from './user.model';
 import { ModelType } from 'typegoose';
 import { UserDTO } from './user.dto';
 import { EntityExceptionCode, EntityException } from '../../exceptions/entity-exception';
+import { InstanceType } from 'typegoose';
 
+/**
+ * Service for manage users save in database
+ */
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>) { }
 
   /**
-   * Create new User in database
+   * Create new User
    * @param user User to insert
    */
-  async insert(user: UserDTO): Promise<UserModel> {
+  async insert(user: UserDTO): Promise<InstanceType<UserModel>> {
     const userCreated = new this.userModel(user);
     return userCreated.save();
   }
 
   /**
-   * Find one article by his ID
+   * Find one user by his ID
    * @param id ID of wanted article
    */
-  async findById(id: string): Promise<UserModel> {
+  async findById(id: string): Promise<InstanceType<UserModel>> {
     const user = await this.userModel.findById(id).exec();
     if (!user) {
       throw new EntityException(EntityExceptionCode.NOT_FOUND);
@@ -31,7 +35,7 @@ export class UsersService {
   }
 
   /**
-   * Find all articles present in database
+   * Find all users
    */
   async findAll(): Promise<UserModel[]> {
     return this.userModel.find({}).exec();
@@ -41,7 +45,7 @@ export class UsersService {
    * Remove user by his ID
    * @param id ID user to delete
    */
-  async remove(id: string): Promise<UserModel> {
+  async remove(id: string): Promise<InstanceType<UserModel>> {
     const deletedUser = await this.userModel.findByIdAndRemove(id).exec();
 
     if (!deletedUser) {
