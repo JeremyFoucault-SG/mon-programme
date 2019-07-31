50f6eeae775b2a151d163778e76f7228f2ab8013
mdhn <maxduchan@gmail.com>
2019-07-31 11:15:37 +0200

    refactoring

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PopupAccueilComponent } from './features/popup-accueil/popup-accueil.component';
-import { PopupHommeComponent } from './features/popup-homme/popup-homme.component';
-import { PopupFemmeComponent } from './features/popup-femme/popup-femme.component';
+import { PopupMwComponent } from './features/popup-mw/popup-mw.component';
 import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
   {path: '', component: PopupAccueilComponent},
-  {path: 'homme', component: PopupHommeComponent},
-  {path: 'femme', component: PopupFemmeComponent},
-  {path: 'home-men', component: HomeComponent},
+  {path: 'homme', component: PopupMwComponent},
+  {path: 'femme', component: PopupMwComponent},
+  {path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
