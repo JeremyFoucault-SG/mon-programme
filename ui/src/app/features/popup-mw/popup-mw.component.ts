cb64ad591dad54402001fd19930c51721da434d4
mdhn <maxduchan@gmail.com>
2019-08-01 02:53:20 +0200

    add documentation

--- ui/src/app/features/popup-mw/popup-mw.component.ts
+++ ui/src/app/features/popup-mw/popup-mw.component.ts
@@ -11,29 +11,71 @@ import { ActivatedRoute, Data } from '@angular/router';
 })
 export class PopupMwComponent implements OnInit {
 
+  /**
+   * True if popup is for choose between men and women
+   * @type {boolean}
+   * @memberof PopupMwComponent
+   */
   @Input()
   isChoose: boolean;
 
+  /**
+   * True for Men template
+   * @type {boolean}
+   * @memberof PopupMwComponent
+   */
   @Input()
   isMen: boolean;
 
+  /**
+   * True for Women template
+   * @type {boolean}
+   * @memberof PopupMwComponent
+   */
   @Input()
   isWomen: boolean;
 
+  /**
+   * Set text content in middle of pupop (over background of men/wonem, not for choose template)
+   * @type {string}
+   * @memberof PopupMwComponent
+   */
+  @Input()
+  textContent: string;
+
+  /**
+   * Set url to switch to women or men template
+   * @type {string}
+   * @memberof PopupMwComponent
+   */
   switchUrl: string;
+
+  /**
+   * Only for title, can take two values : "homme" or "femme"
+   * @type {string}
+   * @memberof PopupMwComponent
+   */
   sex: string;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.setProperty(this.isMen, this.isWomen, this.isChoose);
+    // Set properties according to inputs
+    this.setProperties(this.isMen, this.isWomen, this.isChoose);
 
+    // If component is not a part of another template component (no @Input set), it's route data component binding
     this.activatedRoute.data.subscribe((data: Data) => {
-      this.setProperty(data.isMen, data.isWomen, data.isChoose);
+      this.setProperties(data.isMen, data.isWomen, data.isChoose);
     });
   }
 
-  setProperty(isMen, isWomen, isChoose) {
+  /**
+   *
+   * @param isMen True for men template
+   * @param isWomen True for women template
+   * @param isChoose True for choose template (access to men or women)
+   */
+  setProperties(isMen, isWomen, isChoose) {
     if (isMen) {
       this.switchUrl = '/women';
       this.sex = 'homme';
