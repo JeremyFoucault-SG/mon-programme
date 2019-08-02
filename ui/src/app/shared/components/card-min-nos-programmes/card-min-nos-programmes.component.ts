5b675c51375d399486fb6481795c9994036b3140
mdhn <maxduchan@gmail.com>
2019-08-02 11:58:09 +0200

    thunb card component

--- ui/src/app/shared/components/card-min-nos-programmes/card-min-nos-programmes.component.ts
+++ ui/src/app/shared/components/card-min-nos-programmes/card-min-nos-programmes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-min-nos-programmes',
@@ -6,6 +6,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-min-nos-programmes.component.css']
 })
 export class CardMinNosProgrammesComponent implements OnInit {
+  /**
+   * Set image in background of card
+   */
+  @Input()
+  imageUrl: string;
+
+  /**
+   * If true set a darken overlay above background image
+   */
+  @Input()
+  hasOverlay: boolean;
+
+  /**
+   * If true append a menu (more like) in top-right of card
+   */
+  @Input()
+  hasMenu: boolean;
+
+  /**
+   * Set a tag in top-right of card
+   */
+  @Input()
+  tag: string;
+
+  /**
+   * Set number of stars in top-right of card
+   */
+  @Input()
+  topStars: number;
+
+  /**
+   * Set title of card in left-botton of card
+   */
+  @Input()
+  title: string;
+
+  /**
+   * Set text content of card below title
+   */
+  @Input()
+  content: string;
+
+  /**
+   * If true set title, content, inline stars and add to whishilt inside an overlay trigger on hover
+   */
+  @Input()
+  isContentLayer: boolean;
+
+  /**
+   * Set number on inline start in right-bottom of card
+   */
+  @Input()
+  inlineStars: number;
+
+  /**
+   * If true add button "add to wish list" in right-bottom of card
+   */
+  @Input()
+  hasWish: boolean;
 
   constructor() { }
 
