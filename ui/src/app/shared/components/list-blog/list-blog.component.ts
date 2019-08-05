8b48d9ca3f834ccfa4c45a87994409e9043a8660
Gordian <arragon05@gmail.com>
2019-08-05 09:16:07 +0200

    lint

--- ui/src/app/shared/components/list-blog/list-blog.component.ts
+++ ui/src/app/shared/components/list-blog/list-blog.component.ts
@@ -11,20 +11,28 @@ export class ListBlogComponent implements OnInit {
 
   public articlesBlog = [
     new ArticleBlog('https://zupimages.net/up/19/31/4pis.jpeg', 'Les fruit secs',
+  // tslint:disable-next-line: max-line-length
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dolores, odit quasi possimus molestias enim reprehenderit ea, error sint laborum maxime officia quos harum voluptatum. Quos quidem culpa temporibus placeat eligendi!'),
     new ArticleBlog('https://zupimages.net/up/19/31/4pis.jpeg', 'Les fruit secs',
+  // tslint:disable-next-line: max-line-length
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dolores, odit quasi possimus molestias enim reprehenderit ea, error sint laborum maxime officia quos harum voluptatum. Quos quidem culpa temporibus placeat eligendi!'),
   new ArticleBlog('https://zupimages.net/up/19/31/4pis.jpeg', 'Les fruit secs',
+  // tslint:disable-next-line: max-line-length
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dolores, odit quasi possimus molestias enim reprehenderit ea, error sint laborum maxime officia quos harum voluptatum. Quos quidem culpa temporibus placeat eligendi!'),
     new ArticleBlog('https://zupimages.net/up/19/31/4pis.jpeg', 'Les fruit secs',
+  // tslint:disable-next-line: max-line-length
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dolores, odit quasi possimus molestias enim reprehenderit ea, error sint laborum maxime officia quos harum voluptatum. Quos quidem culpa temporibus placeat eligendi!'),
   new ArticleBlog('https://zupimages.net/up/19/31/4pis.jpeg', 'Les fruit secs',
+  // tslint:disable-next-line: max-line-length
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dolores, odit quasi possimus molestias enim reprehenderit ea, error sint laborum maxime officia quos harum voluptatum. Quos quidem culpa temporibus placeat eligendi!'),
     new ArticleBlog('https://zupimages.net/up/19/31/4pis.jpeg', 'Les fruit secs',
+  // tslint:disable-next-line: max-line-length
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dolores, odit quasi possimus molestias enim reprehenderit ea, error sint laborum maxime officia quos harum voluptatum. Quos quidem culpa temporibus placeat eligendi!'),
   new ArticleBlog('https://zupimages.net/up/19/31/4pis.jpeg', 'Les fruit secs',
+  // tslint:disable-next-line: max-line-length
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dolores, odit quasi possimus molestias enim reprehenderit ea, error sint laborum maxime officia quos harum voluptatum. Quos quidem culpa temporibus placeat eligendi!'),
     new ArticleBlog('https://zupimages.net/up/19/31/4pis.jpeg', 'Les fruit secs',
+  // tslint:disable-next-line: max-line-length
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit.Dolores, odit quasi possimus molestias enim reprehenderit ea, error sint laborum maxime officia quos harum voluptatum. Quos quidem culpa temporibus placeat eligendi!'),
   ];
 
