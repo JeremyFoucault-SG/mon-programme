9cbebb9f677baa20a43d1e0f11d320d83597df70
JeremyFoucault-WCS <jeremfouc@orange.fr>
2019-08-12 08:59:21 +0200

    fix errors for pulling

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -6,12 +6,10 @@ import { HomeComponent } from './features/home/home.component';
 import { ListBlogComponent } from './shared/components/list-blog/list-blog.component';
 import { CardNosProgrammesComponent } from './shared/components/card-nos-programmes/card-nos-programmes.component';
 import { NutritionComponent } from './features/nutrition/nutrition.component';
-// tslint:disable-next-line: max-line-length
 import { FormSelectionProgrammeComponent } from './shared/components/selection-programme/form-selection-programme/form-selection-programme.component';
 import { FunctionSmComponent } from './shared/components/form-function/function-sm/function-sm.component';
 import { FooterSmComponent } from './core/footer/footer-sm/footer-sm.component';
 import { MenuNosProgrammeComponent } from './shared/components/menu-nos-programme/menu-nos-programme.component';
-// tslint:disable-next-line: max-line-length
 import { FormSelectionProgrammeSmComponent } from './shared/components/selection-programme/form-selection-programme-sm/form-selection-programme-sm.component';
 import { ArticleBlogComponent } from './features/article-blog/article-blog.component';
 
@@ -20,7 +18,6 @@ import { ArticleBlogComponent } from './features/article-blog/article-blog.compo
 
 
 const routes: Routes = [
-  { path: '', component: PopupMwComponent },
   {
     path: 'men',
     component: PopupMwComponent,
