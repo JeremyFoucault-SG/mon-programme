ad00e9e62d82bf86d1dd1aa2c4bc33eb27302aaa
JeremyFoucault-WCS <jeremfouc@orange.fr>
2019-08-14 09:22:21 +0200

    création des models: contact, programme, user

---
+++ ui/src/app/shared/models/users.model.ts
@@ -0,0 +1,11 @@
+export class Users {
+    constructor(
+        public firstname: string,
+        public lastname: string,
+        public email: string,
+        public password: string,
+        public age: number,
+        public weight: number,
+        public size: number,
+    ) { }
+}
