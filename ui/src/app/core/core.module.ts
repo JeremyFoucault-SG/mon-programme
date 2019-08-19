a35bd34e8639c437fbdcde3ab9cc30d8100b39d3
Gordian <arragon05@gmail.com>
2019-08-19 10:43:25 +0200

    footer information sm

--- ui/src/app/core/core.module.ts
+++ ui/src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterSmComponent } from './footer/footer-sm/footer-sm.component';
+import { FooterInformationSmComponent } from './footer/footer-information-sm/footer-information-sm.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterSmComponent
+    FooterSmComponent,
+    FooterInformationSmComponent
   ],
   imports: [
     CommonModule
