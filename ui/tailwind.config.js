540f63a52c18c24a4dd8f9a79c0b6366cc5f1b3b
mdhn <maxduchan@gmail.com>
2019-08-27 10:55:31 +0200

    change width of card-thumb

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   theme: {
     fontFamily: {
-     'lato': ['Lato'],
-     'roboto': ['Roboto']
+      'lato': ['Lato'],
+      'roboto': ['Roboto']
+    },
+    extend: {
+      spacing: {
+        '2screen': '200vw',
+        '1/3-screen': 'calc(100vw /3)',
+        '3/4-screen': '75vw'
+      }
     }
   },
   variants: {},
