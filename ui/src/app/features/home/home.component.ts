3bd8a8243af28104b84b940cbb68e354708ff7c0
mdhn <maxduchan@gmail.com>
2019-08-27 10:56:05 +0200

    add all component to home page

--- ui/src/app/features/home/home.component.ts
+++ ui/src/app/features/home/home.component.ts
@@ -11,11 +11,94 @@ export class HomeComponent implements OnInit {
 
   public selected: ProgramDetail;
   public programsInfos: ProgramDetail[] = ProgramsList.infos;
+  public programs = [];
 
   constructor() { }
 
   ngOnInit() {
     this.selected = this.programsInfos[0];
+
+    this.programs = [
+      {
+        title: 'Program 1',
+        content: 'content 1',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+      {
+        title: 'Program 2',
+        content: 'content 2',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+      {
+        title: 'Program 3',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+      {
+        title: 'Program 4',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+    ]
   }
 
   onChange(programDetail: ProgramDetail, index) {
