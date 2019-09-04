ece0f8bed5f96d5f7fa026989b4ccbe08b40f7d3
JeremyFoucault-WCS <jeremfouc@orange.fr>
2019-09-04 15:43:21 +0200

    ajout fonction onsubmit + formgroup et formcontrol

--- ui/src/app/shared/components/form-customize-programme/form-customize-programme.component.ts
+++ ui/src/app/shared/components/form-customize-programme/form-customize-programme.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-form-customize-programme',
@@ -9,15 +11,55 @@ export class FormCustomizeProgrammeComponent implements OnInit {
 
   isHidden = false;
   show = false;
+  customForm: FormGroup;
+  error = '';
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.createForm();
   }
 
-  nextPage() {
+
+  private createForm() {
+  this.customForm = new FormGroup({
+    domicile: new FormControl(''),
+    salle: new FormControl( ''),
+    seances: new FormControl(''),
+    objectif: new FormControl(''),
+    niveau: new FormControl( ''),
+    poids: new FormControl(''),
+    taille: new FormControl( ''),
+    age: new FormControl( ''),
+    pratique: new FormControl(''),
+    muscle: new FormControl( ''),
+  });
+}
+
+  showDetails() {
     this.show = true;
     this.isHidden = !this.isHidden;
+  }
+
+  onFormSubmit() {
+    if (this.customForm.valid) {
+      console.log(this.customForm.value);
+      this.customForm.reset();
+      this.showSuccess();
+    } else {
+      (error: any) => {
+        this.errorSuccess();
+      }
+    }
+
+  }
+
+  showSuccess() {
+    this.toastr.success('Programme Enregistré avec succés.');
+  }
+
+  errorSuccess() {
+    this.toastr.warning('Erreur lors de la sauvegarde')
   }
 
 }
