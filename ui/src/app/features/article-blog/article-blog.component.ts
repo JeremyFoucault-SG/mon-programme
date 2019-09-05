4db2c84e548bcac1a836085386d826fa2e6f5bb9
JeremyFoucault-WCS <jeremfouc@orange.fr>
2019-09-05 14:58:05 +0200

    modif taille texte return page home

--- ui/src/app/features/article-blog/article-blog.component.ts
+++ ui/src/app/features/article-blog/article-blog.component.ts
@@ -3,6 +3,7 @@ import { Store, Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { ArticleState } from 'src/app/core/store/store.module/article/article.state';
 import { GetByIdArticle } from '../../core/store/store.module/article/article.actions';
+import { ProgramDetail, ProgramsList } from 'src/app/shared/models/programs-infos';
 
 @Component({
   selector: 'app-article-blog',
@@ -10,6 +11,11 @@ import { GetByIdArticle } from '../../core/store/store.module/article/article.ac
   styleUrls: ['./article-blog.component.css']
 })
 export class ArticleBlogComponent implements OnInit {
+
+
+public selected: ProgramDetail;
+public programsInfos: ProgramDetail[] = ProgramsList.infos;
+public programs = [];
 
 
   @Input()
@@ -24,6 +30,97 @@ export class ArticleBlogComponent implements OnInit {
 
   ngOnInit() {
     this.store.dispatch(new GetByIdArticle('5d70dfdf42a76f14bf21d70d'));
+    this.selected = this.programsInfos[0];
+
+    this.programs = [
+      {
+        title: 'Program 1',
+        content: 'content 1',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+      {
+        title: 'Program 2',
+        content: 'content 2',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+      {
+        title: 'Program 3',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+      {
+        title: 'Program 4',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+
+      {
+        title: 'Program 5',
+        content: 'content 3',
+        imageUrl: 'https://amp.businessinsider.com/images/5b43ccf31335b831008b4c1c-750-563.jpg'
+      },
+    ];
   }
 
+  onChange(programDetail: ProgramDetail, index) {
+    this.selected = programDetail;
+    console.log();
+  }
+
+
 }
+
+
