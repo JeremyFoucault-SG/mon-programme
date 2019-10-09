5adb478ed048a86d4ca0af6c100ea78129793d7d
JeremyFoucault-WCS <jeremfouc@orange.fr>
2019-10-09 09:37:24 +0200

    ajout validator + routing commande et paiment

--- ui/src/app/features/commande/commande.component.ts
+++ ui/src/app/features/commande/commande.component.ts
@@ -6,6 +6,10 @@ import { Cart } from 'src/app/shared/models/cart.model';
 import { GetAllCarts, DeleteCartCoaching } from 'src/app/core/store/store.module/cart/cart.actions';
 import { SumPipe } from '../../shared/pipes/sum.pipe';
 import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/core/authentication/authentication.service';
+import { UsersService } from 'src/app/core/http/users.service';
 
 @Component({
   selector: 'app-commande',
@@ -18,24 +22,47 @@ export class CommandeComponent implements OnInit {
   paiement = false;
   isHidden = false;
   pipes: [SumPipe];
+  model: any = {};
+  public myForm: FormGroup;
+  loading = false;
 
 
-
-
-  constructor(private store: Store, private toastr: ToastrService) {
+  constructor(private store: Store,
+              private router: Router,
+              private usersService: UsersService,
+              private toastr: ToastrService) {
   }
 
   @Select(CartState.cartCoachings)
   carts: Observable<Cart[]>;
 
 
-
   ngOnInit() {
     this.store.dispatch(new GetAllCarts());
+    this.myForm = new FormGroup({
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
   }
   deleteCartCoaching(id: string) {
     this.store.dispatch(new DeleteCartCoaching(id));
     this.toastr.warning('Programme supprimé du panier !');
   }
 
+
+  onSubmit(payload) {
+    this.loading = true;
+    this.usersService.addUserInfos(payload).subscribe(
+      () => {
+        this.router.navigate(['paiement']);
+      },
+      (error) => {
+        this.loading = false;
+        this.toastr.error('Erreur', 'tous les champs ne sont pas saisis', {
+        });
+      },
+    );
+  }
 }
