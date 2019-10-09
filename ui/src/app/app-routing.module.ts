d649ff152683ca7265f0225d4591196879d8862f
Gordian05 <arragon05@gmail.com>
2019-10-09 11:55:48 +0200

    lint

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -119,7 +119,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{
+  imports: [RouterModule.forRoot(routes, {
     scrollPositionRestoration: 'enabled'
   })],
   exports: [RouterModule]
