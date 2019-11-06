7e3d5566c2de9a5879e1c4179632939180d6cba4
Gordian05 <arragon05@gmail.com>
2019-11-06 10:29:42 +0100

    load bookmark startup

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, Select } from '@ngxs/store';
+import { WishState } from './core/store/store.module/wishe/wish.state';
+import { Observable } from 'rxjs';
+import { Wish } from './shared/models/wishes.model';
+import { GetAllWishesCoaching, GetAllWishesArticles } from './core/store/store.module/wishe/wish.action';
+import { AuthenticationService } from './core/authentication/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ui';
+export class AppComponent implements OnInit {
+  constructor(private store: Store, private authService: AuthenticationService) {}
+
+
+  ngOnInit() {
+    if (this.authService.isLogin()) {
+      this.store.dispatch(new GetAllWishesCoaching());
+      this.store.dispatch(new GetAllWishesArticles());
+    }
+
+  }
 }
